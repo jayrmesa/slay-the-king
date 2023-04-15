243c38b76fd870e89a1df1c21a90982d2c1d15d8
Lisha Liang <lishaliang@Lishas-MacBook-Pro.local>
2023-04-15 13:32:10 -0400

    gameover pane css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './components/screens/mainMenu';
 import CharacterSelection from './components/screens/CharacterSelection';
 import ChoiceRoom from './components/game/choiceRoom';
 import Map from './components/game/Map';
 import BattleRoom from "./components/game/BattleRoom";
+import GameOver from './components/GameOver';
 
 import './App.css';
 
 function App() {
+  const [gameOver, setGameOver] = useState(false);
+  const score = 100;
+
+  const handleExitGame = () => {
+    setGameOver(false);
+  }
+
+  const handlePlayAgain = () => {
+    setGameOver(false);
+    // Reset game state or start a new game
+  }
 
   return (
     <div className="App">
+        
+      <h1>Game Over</h1>
+      {gameOver ? (
+        <GameOver score={score} exitGame={handleExitGame} playAgain={handlePlayAgain} />
+      ) : (
+        <button onClick={() => setGameOver(true)}>Game Over</button>
+      )}
+  
       <Router>
         <Routes>
           <Route path="/" element={<Menu />} />
--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,15 +1,15 @@
-import React from 'react';
-
+import './gameover.css';
 const GameOver = (props) => {
   const {score, exitGame, playAgain} = props
   return (
-    <div>
-      <h2>Game Over!</h2>
-      <p>Your score: {score}</p>
-      <button onClick={exitGame}>Exit</button>
-      <button onClick={playAgain}>Play Again</button>
+    <div className="game-over-container">
+      <h2 className="game-over-title">Game Over!</h2>
+      <p className="game-over-score">Your score: {score}</p>
+      <button className="game-over-button exit-button" onClick={exitGame}>Exit</button>
+      <button className="game-over-button play-again-button" onClick={playAgain}>Play Again</button>
     </div>
   );
 }
 
 export default GameOver;
+
