802aaf3d38f6db2018f941a3d1f85103cbc02067
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-04-17 11:30:21 -0600

    updated the server folder and installed express and cors in the root

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
+// server/index.js
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from the server!");
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
