ff507b794bc00a553967310cfcc5409cb2d0ce4f
Charlie <charlieryancurry@gmail.com>
2023-04-20 11:09:38 -0700

    create unfinished page for character decks

--- src/components/screens/deck.js
+++ src/components/screens/deck.js
@@ -1,22 +1,37 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+import { cardDecks } from '../common/cardDecks';
+
+const ImgDeck = (props) => {
+
+  const { deck = [] } = props;
+
+  return (
+    <>
+      {
+        deck.map(card => (
+          <img
+            key={card.id}
+            className="card"
+            src={card.image}
+            alt={`Card ${card.id}`}
+          />
+        ))
+      }
+    </>
+  )
+}
 
 const Deck = () => {
-  const location = useLocation();
 
-  const { selectedCharacter } = location.state || {};
+  const cardDeckValues = Object.values(cardDecks);
 
   return (
     <div className="card-deck">
-    {selectedCharacter.startingDeck.map((card) => (
-      <img
-        key={card.id}
-        className="card"
-        src={card.image}
-        alt={`Card ${card.id}`}
-      />
-    ))}
-  </div>
+      {
+        cardDeckValues.map(deck => (<ImgDeck deck={deck}/>))
+      }
+    </div>
   )
 }
 
